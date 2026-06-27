e73094210f7f5995a0453064b9f94f1ce37c0920
AtifAziz600 <feralligatrfahim630@gmail.com>
2026-06-27 15:23:37 +0600

    f

--- Module-21-22/Module-22/src/Hook-Lecture/UseEffectunderstandingargumentsanduses.jsx
+++ Module-21-22/Module-22/src/Hook-Lecture/UseEffectunderstandingargumentsanduses.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 const UseEffectunderstandingargumentsanduses = () => {
     useEffect(() => {
         console.log("Computed Mounted")
-    }, [1])
+    }, [])
     return (
         <div>
             
